046eb41fe8044d689d74cf53f04d0d5642daa206
foestauf <46908845+foestauf@users.noreply.github.com>
2020-07-16 09:27:35 -0400

    Initial

--- src/App.js
+++ src/App.js
@@ -2,25 +2,39 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { useFirestoreDocData, useFirestore, SuspenseWithPerf} from 'reactfire';
+
+
+function Burrito() {
+  // lazy load the Firestore SDK and create a document reference
+  const burritoRef = useFirestore()
+      .collection('tryreactfire')
+      .doc('burrito');
+
+  // subscribe to the doc. just one line!
+  const burrito = useFirestoreDocData(burritoRef);
+
+  // get the value from the doc
+  const isYummy = burrito.yummy;
+
+  return <p>The burrito is {isYummy ? 'good' : 'bad'}</p>;
+}
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="App">
+        {/*
+        SuspenseWithPerf behaves the same as Suspense,
+        but also automatically measures load times with the User Timing API
+        and reports it to Firebase Performance Monitoring
+      */}
+        <SuspenseWithPerf
+            fallback={'loading burrito status...'}
+            traceId={'load-burrito-status'}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <Burrito />
+        </SuspenseWithPerf>
+      </div>  );
 }
 
 export default App;
